1a5ff45a39454bdc7df13b265ab48dfac142c461
Christian Kerl <christian-kerl@web.de>
2010-10-04 21:13:45 +0200

    added DependencyInjection configuration for SoapKernel

--- DependencyInjection/WebServiceExtension.php
+++ DependencyInjection/WebServiceExtension.php
@@ -2,6 +2,8 @@
 
 namespace Bundle\WebServiceBundle\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -9,7 +11,10 @@ class WebServiceExtension extends Extension
 {
     public function configLoad(array $config, ContainerBuilder $configuration)
     {
-        $configuration->getDefinition("request")->setClass("Bundle\\WebServiceBundle\\SoapRequest");
+        $loader = new XmlFileLoader($configuration, __DIR__ . "/../Resources/config");
+        $loader->load("services.xml");
+
+        $configuration->setAlias("http_kernel", "webservice_http_kernel");
     }
 
     public function getXsdValidationBasePath()
--- SoapKernel.php
+++ SoapKernel.php
@@ -2,9 +2,22 @@
 
 namespace Bundle\WebServiceBundle;
 
-use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpFoundation\Request;
 
-class SoapKernel extends HttpKernel
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+
+class SoapKernel extends ContainerAware implements HttpKernelInterface
 {
+    public function getRequest()
+    {
+        return null;
+    }
 
+    public function handle(Request:: $request = null, $type = self::MASTER_REQUEST, $raw = false)
+    {
+        $this->container->getSymfonyHttpKernelService()->handle($request, $type, $raw);
+    }
 }
