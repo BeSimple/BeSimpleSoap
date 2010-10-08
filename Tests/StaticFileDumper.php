c7f173456b2997ef4f2f4ba23865305e78792d30
Christian Kerl <christian-kerl@web.de>
2010-10-08 17:01:27 +0200

    added header comment

--- Tests/StaticFileDumper.php
+++ Tests/StaticFileDumper.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the WebServiceBundle.
+ *
+ * (c) Christian Kerl <christian-kerl@web.de>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Bundle\WebServiceBundle\Tests;
 
