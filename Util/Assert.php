4d8fae2fbe640a89cd612fe50ea86a901b8ec833
Christian Kerl <christian-kerl@web.de>
2011-01-07 23:01:34 +0100

    added Assert class to simplify method parameter validation

---
+++ Util/Assert.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * This file is part of the WebServiceBundle.
+ *
+ * (c) Christian Kerl <christian-kerl@web.de>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Bundle\WebServiceBundle\Util;
+
+/**
+ *
+ * @author Christian Kerl <christian-kerl@web.de>
+ */
+class Assert
+{
+    const ARGUMENT_INVALID = "Argument '%s' is invalid!";
+    const ARGUMENT_NULL = "Argument '%s' can't be null!";
+
+    public static function thatArgument($name, $condition, $message = self::ARGUMENT_INVALID)
+    {
+        if(!$condition)
+        {
+            throw new \InvalidArgumentException(sprintf($message, $name));
+        }
+    }
+
+    public static function thatArgumentNotNull($name, $value)
+    {
+        self::thatArgument($name, $value != null, self::ARGUMENT_NULL);
+    }
+}
