3141adcfadcd28917e180e56ddaf4b298ab570a3
Francis Besset <francis.besset@gmail.com>
2011-08-27 18:09:48 +0200

    Fixed namespace

--- Tests/Converter/DateTimeTypeConverterTest.php
+++ Tests/Converter/DateTimeTypeConverterTest.php
@@ -8,7 +8,7 @@
  * with this source code in the file LICENSE.
  */
 
-namespace BeSimple\SoapBundle\Tests\ServiceBinding;
+namespace BeSimple\SoapBundle\Tests\Converter;
 
 use BeSimple\SoapBundle\Soap\SoapRequest;
 use BeSimple\SoapBundle\Soap\SoapResponse;
@@ -24,8 +24,8 @@ class DateTimeTypeConverterTest extends \PHPUnit_Framework_TestCase
     public function testConvertXmlToPhp()
     {
         $converter = new DateTimeTypeConverter();
-        $dateXml = '<sometag>2002-10-10T12:00:00-05:00</sometag>';
 
+        $dateXml = '<sometag>2002-10-10T12:00:00-05:00</sometag>';
         $date = $converter->convertXmlToPhp(new SoapRequest(), $dateXml);
 
         $this->assertEquals(new \DateTime('2002-10-10T12:00:00-05:00'), $date);
@@ -34,8 +34,8 @@ class DateTimeTypeConverterTest extends \PHPUnit_Framework_TestCase
     public function testConvertPhpToXml()
     {
         $converter = new DateTimeTypeConverter();
-        $date = new \DateTime('2002-10-10T12:00:00-05:00');
 
+        $date    = new \DateTime('2002-10-10T12:00:00-05:00');
         $dateXml = $converter->convertPhpToXml(new SoapResponse(), $date);
 
         $this->assertEquals('<dateTime>2002-10-10T12:00:00-05:00</dateTime>', $dateXml);
