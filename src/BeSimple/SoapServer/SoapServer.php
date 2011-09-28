61946b30f8dd8a59147f9516b10015a3c89bd407
Christian Kerl <christian-kerl@web.de>
2011-09-28 13:56:29 +0200

    initial checkin

---
+++ src/BeSimple/SoapServer/SoapServer.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the BeSimpleSoapServer.
+ *
+ * (c) Christian Kerl <christian-kerl@web.de>
+ * (c) Francis Besset <francis.besset@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace BeSimple\SoapServer;
+
+/**
+ * @author Christian Kerl
+ */
+class SoapServer extends \SoapServer
+{
+	
+}
