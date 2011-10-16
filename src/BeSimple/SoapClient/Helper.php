98a916957cda0434acaea8a7ef3b58a2d4c1d8f2
Andreas Schamberger <mail@andreass.net>
2011-10-16 17:18:35 +0200

    fix file header

--- src/BeSimple/SoapClient/Helper.php
+++ src/BeSimple/SoapClient/Helper.php
@@ -8,8 +8,6 @@
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
- *
- * @link https://github.com/BeSimple/BeSimpleSoapClient
  */
 
 namespace BeSimple\SoapClient;
