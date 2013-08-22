0d4be76389d57fb00f6531ec9f6651e808c8d707
Hector Hurtarte <hectorh30@gmail.com>
2013-08-21 22:51:25 -0600

    Add 'encoding' option to allow overriding server encoding

--- src/BeSimple/SoapBundle/DependencyInjection/Configuration.php
+++ src/BeSimple/SoapBundle/DependencyInjection/Configuration.php
@@ -117,6 +117,8 @@ class Configuration
                                     ->thenInvalid(sprintf('The cache type has to be either %s', implode(', ', $this->cacheTypes)))
                                 ->end()
                             ->end()
+                            ->scalarNode('encoding')
+                            ->end()
                         ->end()
                     ->end()
                 ->end()
