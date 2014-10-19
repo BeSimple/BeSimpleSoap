7ce4481a738c71e7ec667c8dfbfe9f48815941ce
Sergey Zhuravel <zhuravel.sergey@gmail.com>
2014-10-19 21:25:26 +0300

    Fix methods declarations

--- src/BeSimple/SoapBundle/ServiceBinding/DocumentLiteralWrappedRequestMessageBinder.php
+++ src/BeSimple/SoapBundle/ServiceBinding/DocumentLiteralWrappedRequestMessageBinder.php
@@ -11,13 +11,17 @@
 namespace BeSimple\SoapBundle\ServiceBinding;
 
 use BeSimple\SoapBundle\ServiceDefinition\Method;
+use BeSimple\SoapCommon\Definition\Type\TypeRepository;
 
 /**
  * @author Christian Kerl <christian-kerl@web.de>
  */
 class DocumentLiteralWrappedRequestMessageBinder implements MessageBinderInterface
 {
-    public function processMessage(Method $messageDefinition, $message)
+    /**
+     * {@inheritdoc}
+     */
+    public function processMessage(Method $messageDefinition, $message, TypeRepository $typeRepository)
     {
         if(count($message) > 1) {
             throw new \InvalidArgumentException();
