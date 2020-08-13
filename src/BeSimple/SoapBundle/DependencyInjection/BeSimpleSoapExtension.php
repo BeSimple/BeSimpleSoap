d25540371b1f6a26ef714b1c31e6899b5a994b54
Scott Beaman <scottbeaman@wirelesslogic.com>
2020-08-13 08:45:29 +0100

    Symfony 4 compatibility

--- src/BeSimple/SoapBundle/DependencyInjection/BeSimpleSoapExtension.php
+++ src/BeSimple/SoapBundle/DependencyInjection/BeSimpleSoapExtension.php
@@ -17,7 +17,7 @@ use BeSimple\SoapCommon\Cache;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\ChildDefinition as DefinitionDecorator;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
